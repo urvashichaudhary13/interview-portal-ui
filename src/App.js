d5166ddecb6fab1505f0d1935f9332069c23bf4a
urvashichaudhary13 <urvashichaudhary2513@gmail.com>
2023-01-14 19:14:04 +0530

    Login and Signup UI and routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Login, Signup  } from "./pages";
 
 export const App = () => {
   return(
-    <h1>INTERVIEW PORTAL: UI</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+          <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
+
 
 export default App;
---
+++ src/pages/Signup/signup.js
@@ -0,0 +1,30 @@
+import { Header } from "../../component/Header"
+import "./style.css";
+
+export const Signup = () => {
+    return(
+        <>
+        <Header />
+        <section>
+            <div className="box card">
+                <form className="box-input">
+                <input class="form-control" placeholder="First name" required />
+                <br />
+                <input class="form-control" placeholder="Last name" required />
+                <br />
+                <input type="email" placeholder="Email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required />
+                <br />
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required/>
+                <br />
+                <button type="submit" class="btn btn-primary login-button">Sign Up</button>
+                <label>Already a member?</label>
+                <a href="/login">Sign in</a>
+                <br />
+                </form>
+            </div>
+        </section>
+        </>
+    )
+}
+
+export default Signup;
