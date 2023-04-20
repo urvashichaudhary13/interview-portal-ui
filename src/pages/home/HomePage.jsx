091630afe6b4185625985e850ede9f2d8a345bd5
urvashichaudhary13 <urvashichaudhary2513@gmail.com>
2023-04-20 19:03:05 +0530

    Login, Signup, Add candidate API, Delete API

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,33 +1,80 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Header, Stats, Feedback } from "../../components";
-import { tableData } from "../../mocks";
+import { Dropdown } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 import "./style.css";
 
 export const HomePage = () => {
-
+  const [isOpen, setOpen] = useState(false);
   let selected = 0;
   let aligned = 0;
   let rejected = 0;
-  const [data,setData] = useState(tableData)
-  const total = data.length;
+  const [data, setData] = useState([]);
+  const [isRefetch, setRefetch] = useState(false);
+  const total = data?.length;
 
-  const tableRows = data.map((data, index) => {
-    if(data.status === 'Selected'){
-      selected= selected+1;
-    } else if(data.status === 'Aligned'){
-      aligned=aligned+1
+  useEffect(() => {
+    if (data) {
+      getTableData();
+      setRefetch(false);
+    }
+  }, [isRefetch]);
+
+  const getTableData = async () => {
+    let tableData = await fetch("http://localhost:3006/api/candidate", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "http://localhost:3006",
+      },
+    });
+    tableData = await tableData.json();
+    setData(tableData);
+  };
+
+  const handleDelete = async(id) => {
+    console.log("e--------",id)
+    let dataToDelete = await fetch(`http://localhost:3006/api/candidate/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "http://localhost:3006",
+      },
+    });
+    dataToDelete = await dataToDelete.json()
+    console.log("dataToDelete:::::::::::::", dataToDelete)
+  }
+
+  const tableRows = data?.map((data, index) => {
+    if (data.status === "Selected") {
+      selected = selected + 1;
+    } else if (data.status === "Aligned") {
+      aligned = aligned + 1;
     } else {
-      rejected=rejected+1;
+      rejected = rejected + 1;
     }
     return (
       <tr>
-        <th scope="row">{index+1}</th>
+        <th scope="row">{index + 1}</th>
         <td>{data.name}</td>
         <td>{data.email}</td>
         <td>{data.experience} years</td>
         <td>{data.status}</td>
         <td>{data.department}</td>
-        <td><b><i class="bi bi-three-dots"></i></b></td>
+        <td>
+          <Dropdown trigger="hover">
+            <Dropdown.Toggle
+              variant="none"
+              className="dropdown-toggle"
+            >
+              &#8226; &#8226; &#8226;
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Feedback</Dropdown.Item>
+              <Dropdown.Item href="#action-2"><Button onClick={() => handleDelete(data._id)}>Delete</Button></Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </td>
       </tr>
     );
   });
@@ -35,7 +82,7 @@ export const HomePage = () => {
   const statsData = [
     {
       count: selected,
-      title: "Selected"
+      title: "Selected",
     },
     {
       count: rejected,
@@ -46,12 +93,10 @@ export const HomePage = () => {
       title: "Aligned",
     },
     {
-      count:total,
+      count: total,
       title: "Interviews",
-    }
-  ]
-
-
+    },
+  ];
 
   return (
     <>
@@ -59,17 +104,28 @@ export const HomePage = () => {
       <br />
       <section class="divs">
         {statsData.map((data) => (
-            <Stats 
-            count={data.count}
-            title={data.title}
-             />
+          <Stats count={data.count} title={data.title} />
         ))}
       </section>
       <div class="table-size">
-      <button type="button" class="btn add-candidate" data-bs-toggle="modal" data-bs-target="#myModal">Add candidate</button>
-      <Feedback data={data} setData={setData}/>
-      <br />
-      <br />
+        <Button
+          variant="secondary"
+          onClick={() => {
+            setOpen(!isOpen);
+          }}
+        >
+          Add candidate
+        </Button>
+        {isOpen && (
+          <Feedback
+            data={data}
+            isOpen={isOpen}
+            setOpen={setOpen}
+            setRefetch={setRefetch}
+          />
+        )}
+        <br />
+        <br />
         <table className="table table-hover table-bordered">
           <thead>
             <tr>
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,14 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Header } from "../../components";
 import "./login.css";
 
 const Login = () => {
+  const [state, setState] = useState({
+    email:"",
+    password:""
+  })
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    let result = await fetch("http://localhost:3006/api/users/login", {
+      method:"POST",
+      body: JSON.stringify(e),
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "http://localhost:3006"
+      }
+    })
+    result = await result.json();
+    if(result.error) return
+    navigate("/home")
+  }
+  const handleChange = (selected, type) => {
+    if(type === "email") {
+      setState({
+        ...state,
+        email: selected,
+      })
+    } else {
+      setState({
+        ...state,
+        password:selected
+      })
+    }
+  }
   return (
     <>
       <Header />
       <section>
         <div className="login-box card">
-          <form className="box-input">
+          <form className="box-input" onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit(state)}
+          }>
           <label class="login-title"><h3><b>Login</b></h3></label>
           <br />
             <label>New User?</label>
@@ -24,6 +60,7 @@ const Login = () => {
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
               required
+              onChange={(e) => handleChange(e.target.value, "email")}
             />
             <br />
             <input
@@ -32,6 +69,7 @@ const Login = () => {
               id="exampleInputPassword1"
               placeholder="Password"
               required
+              onChange={(e) => handleChange(e.target.value, "password")}
             />
             <br />
             <button type="submit" class="btn btn-primary login-button">
